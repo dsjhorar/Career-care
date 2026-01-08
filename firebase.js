15130f4df3e25631dcd320eb1b82fba315e0a766
dsjhorar <87761489+dsjhorar@users.noreply.github.com>
2026-01-08 18:24:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@
 
 <script>
 var firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  databaseURL: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyBCuefZt2WuNsNiFme8M-HUHm_BZYKJAi4",
+  authDomain: "career-care-afb64.firebaseapp.com",
+  databaseURL: "https://career-care-afb64-default-rtdb.firebaseio.com",
+  projectId: "career-care-afb64",
+  storageBucket: "career-care-afb64.firebasestorage.app",
+  messagingSenderId: "267890746286",
+  appId: "1:267890746286:web:2c38f0980119d01b145e3f"
 };
 
 firebase.initializeApp(firebaseConfig);
